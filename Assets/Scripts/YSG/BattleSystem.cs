d551929523ab05ac2c0ed875178f8a5c9851a7e8
yang491658 <park49164357@gmail.com>
2025-07-17 10:00:59 +0900

    Human 스크립트 반영

--- Assets/Scripts/YSG/BattleSystem.cs
+++ Assets/Scripts/YSG/BattleSystem.cs
@@ -4,14 +4,14 @@ using UnityEngine;
 
 public class BattleSystem : MonoBehaviour
 {
-    public List<TestCard> playerCards = new List<TestCard>();
+    public List<Human> humanCards = new List<Human>();
     public List<TestCard> monsterCards = new List<TestCard>();
 
     private bool inBattle = false;
 
     private void Start()
     {
-        TestCard[] allCards = FindObjectsByType<TestCard>(FindObjectsSortMode.None);
+        Card2D[] allCards = FindObjectsByType<Card2D>(FindObjectsSortMode.None);
         System.Array.Sort(allCards, (a, b) => a.name.CompareTo(b.name));
 
         foreach (var card in allCards)
@@ -21,11 +21,11 @@ public class BattleSystem : MonoBehaviour
                 switch (characterCard.characterType)
                 {
                     case CharacterType.Human:
-                        playerCards.Add(card);
+                        humanCards.Add(card.GetComponent<Human>());
                         break;
 
                     case CharacterType.Monster:
-                        monsterCards.Add(card);
+                        monsterCards.Add(card.GetComponent<TestCard>());
                         break;
                 }
             }
@@ -34,7 +34,7 @@ public class BattleSystem : MonoBehaviour
 
     private void Update()
     {
-        playerCards.RemoveAll(card => card == null);
+        humanCards.RemoveAll(card => card == null);
         monsterCards.RemoveAll(card => card == null);
 
         if (Input.GetKeyUp(KeyCode.T))
@@ -48,23 +48,21 @@ public class BattleSystem : MonoBehaviour
     {
         inBattle = true;
 
-        foreach (var p in playerCards)
+        foreach (var p in humanCards)
         {
             if (monsterCards.Count == 0) break;
             int randIndex = Random.Range(0, monsterCards.Count);
-            TestCard target = monsterCards[randIndex];
-            yield return StartCoroutine(p.AttackEffect(target));
+            var target = monsterCards[randIndex];
+            yield return StartCoroutine(AttackEffect(p, target));
             yield return new WaitForSeconds(0.1f);
         }
 
-        yield return new WaitForSeconds(0.5f);
-
         foreach (var m in monsterCards)
         {
-            if (playerCards.Count == 0) break;
-            int randIndex = Random.Range(0, playerCards.Count);
-            TestCard target = playerCards[randIndex];
-            yield return StartCoroutine(m.AttackEffect(target));
+            if (humanCards.Count == 0) break;
+            int randIndex = Random.Range(0, humanCards.Count);
+            Human target = humanCards[randIndex];
+            yield return StartCoroutine(AttackEffect(m, target));
             yield return new WaitForSeconds(0.1f);
         }
 
@@ -74,10 +72,57 @@ public class BattleSystem : MonoBehaviour
         inBattle = false;
     }
 
+    private IEnumerator AttackEffect(Character attacker, Character target)
+    {
+        Transform attackerTr = attacker.transform;
+        Transform targetTr = target.transform;
+
+        Vector3 originPos = attackerTr.position;
+        Vector3 targetPos = targetTr.position;
+        Vector3 midPos = Vector3.Lerp(originPos, targetPos, 0.8f);
+
+        float t = 0;
+        while (t < 1)
+        {
+            t += Time.deltaTime * 10;
+            attackerTr.position = Vector3.Lerp(originPos, midPos, t);
+            yield return null;
+        }
+
+        yield return StartCoroutine(HitEffect(target));
+
+        t = 0;
+        while (t < 1)
+        {
+            t += Time.deltaTime * 10;
+            attackerTr.position = Vector3.Lerp(midPos, originPos, t);
+            yield return null;
+        }
+
+        Debug.Log($"{attacker.name}가 {target.name}를 공격 : 데미지 {attacker.charData.attack_power} / 타겟 체력 {target.currentHealth}");
+        target.TakeDamage(attacker.charData.attack_power);
+    }
+
+    private IEnumerator HitEffect(Character target)
+    {
+        SpriteRenderer sr = target.GetComponent<SpriteRenderer>();
+        if (sr == null) yield break;
+
+        Color originalColor = sr.color;
+
+        for (int i = 0; i < 2; i++)
+        {
+            sr.color = Color.clear;
+            yield return new WaitForSeconds(0.1f);
+            sr.color = originalColor;
+            yield return new WaitForSeconds(0.1f);
+        }
+    }
+
     private void DebugResult()
     {
         string playerHealthLog = "플레이어 체력 : ";
-        foreach (var p in playerCards)
+        foreach (var p in humanCards)
             playerHealthLog += $"{p.currentHealth} / ";
         playerHealthLog = playerHealthLog.TrimEnd(' ', '/');
         Debug.Log(playerHealthLog);
--- Assets/Scripts/YSG/TestCard.cs
+++ Assets/Scripts/YSG/TestCard.cs
@@ -1,13 +1,9 @@
-using System.Collections;
 using UnityEngine;
 
-public class TestCard : Card2D
+public class TestCard : Character
 {
-    private SpriteRenderer sr;
-
     [Header("Ä«µå ½ºÅÈ")]
     public float maxHealth;
-    public float currentHealth;
     public float attackPower;
     public float defensePower;
 
@@ -15,63 +11,18 @@ public class TestCard : Card2D
 
     private void Awake()
     {
-        sr = GetComponent<SpriteRenderer>();
+        charData = GetComponent<Card2D>().cardData as CharacterCardData;
 
-        CharacterCardData characterData = cardData as CharacterCardData;
-        if (characterData != null)
+        if (charData != null)
         {
-            maxHealth = characterData.max_health;
-            currentHealth = characterData.max_health;
-            attackPower = characterData.attack_power;
-            defensePower = characterData.defense_power;
+            maxHealth = charData.max_health;
+            currentHealth = charData.max_health;
+            attackPower = charData.attack_power;
+            defensePower = charData.defense_power;
         }
     }
 
-    public IEnumerator AttackEffect(TestCard target)
-    {
-        Vector3 originPos = transform.position;
-        Vector3 targetPos = target.transform.position;
-        Vector3 midPos = Vector3.Lerp(originPos, targetPos, 0.8f);
-
-        float t = 0;
-        while (t < 1)
-        {
-            t += Time.deltaTime * 10;
-            transform.position = Vector3.Lerp(originPos, midPos, t);
-            yield return null;
-        }
-
-        yield return target.HitEffect();
-
-        t = 0;
-        while (t < 1)
-        {
-            t += Time.deltaTime * 10;
-            transform.position = Vector3.Lerp(midPos, originPos, t);
-            yield return null;
-        }
-
-        Debug.Log($"{name}°¡ {target.name}¸¦ °ø°Ý!");
-
-        target.TakeDamage(attackPower);
-    }
-
-    public IEnumerator HitEffect()
-    {
-        if (sr == null) yield break;
-
-        Color originalColor = sr.color;
-
-        for (int i = 0; i < 2; i++)
-        {
-            sr.color = Color.clear;
-            yield return new WaitForSeconds(0.1f);
-            sr.color = originalColor;
-            yield return new WaitForSeconds(0.1f);
-        }
-    }
-
-    public void TakeDamage(float _damage)
+    public override void TakeDamage(float _damage)
     {
         currentHealth -= _damage;
 
@@ -79,7 +30,7 @@ public class TestCard : Card2D
             Die();
     }
 
-    public void Die()
+    public override void Die()
     {
         Debug.Log(name + " »ç¸Á");
 
