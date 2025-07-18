fd45999e5040a12fa2835cef1a48b4b43c893ee6
yang491658 <park49164357@gmail.com>
2025-07-18 14:30:05 +0900

    Update BattleManager.cs

--- Assets/Scripts/YSG/BattleManager.cs
+++ Assets/Scripts/YSG/BattleManager.cs
@@ -202,7 +202,7 @@ public class BattleManager : MonoBehaviour
         }
 
         attacker.Attack(target);
-        Debug.Log($"{attacker.name}가 {target.name}를 공격 : 데미지 {attacker.charData.attack_power} / 타겟 체력 {target.currentHealth}");
+        Debug.Log($"{attacker.name}가 {target.name}를 공격 : 타겟 {target.currentHealth} - {attacker.charData.attack_power}");
 
         ArrangeCharacters();
     }
