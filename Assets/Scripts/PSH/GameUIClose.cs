1b2874583e2311b0116f623a9b3d420ba5ab7c56
PARKSH0912 <kkqwert882950@gmail.com>
2025-08-17 15:08:13 +0900

    11

---
+++ Assets/Scripts/PSH/GameUIClose.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameUIClose : MonoBehaviour
+{
+
+    [SerializeField] private GameObject targetPanel; // 켜고 싶은 UI 패널
+
+    public void OpenUI()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (targetPanel != null)
+                targetPanel.SetActive(true);
+        }
+    }
+
+    public void OnReStartButton()
+    {
+        if (targetPanel != null)
+            targetPanel.SetActive(false);
+    }
+
+    public void OnTitleButton()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    public void OnExitButton()
+    {
+        Application.Quit();
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false; // �����Ϳ����� ���� ����
+#endif
+    }
+}
